6664d9fcfdf623b36460b27f52f30801e1a1a2d0
Ian Boyle <irboyle21@gmail.com>
2021-11-16 11:25:14 -0500

    adds showMovie method

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,87 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" />
-    <p>{{ message }}</p>
+    <h1>{{ message }}</h1>
+    <div class="movies-index">
+      <p>{{ message }}</p>
+      <div v-for="movie in movies" :key="movie.id">
+        <p>{{ movie.title }}</p>
+        <button v-on:click="showMovie(movie)">Movie Info</button>
+      </div>
+      <dialog id="movie-details">
+        <form method="dialog">
+          <p>Title: {{ currentMovie.title }}</p>
+          <p>Year: {{ currentMovie.year }}</p>
+          <p>Plot: {{ currentMovie.plot }}</p>
+          <button>Close</button>
+        </form>
+      </dialog>
+    </div>
+    <div class="movies-new">
+      <form v-on:submit.prevent="createMovie()">
+        <h1>New Movie!</h1>
+        <ul>
+          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        </ul>
+        <div>
+          <label>Title:</label>
+          <input type="text" v-model="newMovieParams.title" />
+        </div>
+        <div>
+          <label>Year:</label>
+          <input type="text" v-model="newMovieParams.year" />
+        </div>
+        <div>
+          <label>Plot:</label>
+          <input type="text" v-model="newMovieParams.plot" />
+        </div>
+        <div>
+          <label>Director:</label>
+          <input type="text" v-model="newMovieParams.director" />
+        </div>
+        <div>
+          <label>In English?:</label>
+          <input type="text" v-model="newMovieParams.english" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import axios from "axios";
 export default {
   data: function () {
     return {
       message: "Welcome to my movie app.",
+      movies: [],
+      newMovieParams: {},
+      errors: {},
+      currentMovie: {},
     };
+  },
+  created: function () {
+    axios.get("http://localhost:3000/movies").then((response) => {
+      this.movies = response.data;
+      console.log("Movies:", response.data);
+    });
+  },
+  methods: {
+    createMovie: function () {
+      axios
+        .post("http://localhost:3000/movies", this.newMovieParams)
+        .then(() => {
+          this.$router.push("/movies");
+          console.log("Success!");
+        })
+        .catch((error) => console.log(error.response));
+    },
+    showMovie: function (movie) {
+      this.currentMovie = movie;
+      document.querySelector("#movie-details").showModal();
+    },
   },
 };
 </script>
